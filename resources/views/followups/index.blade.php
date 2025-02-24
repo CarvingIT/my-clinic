54cbbc974d5a6a9afdd8560fb83c19b6a3afa0db
dhananj001 <dhananjayborse001@gmail.com>
2025-02-24 16:41:37 +0530

    Update index.blade.php
    added the condition if patient is null then it won't be shown

--- resources/views/followups/index.blade.php
+++ resources/views/followups/index.blade.php
@@ -28,41 +28,41 @@
 
                                 </tr>
                             </thead>
-                            <tbody class="bg-white dark:bg-gray-900 divide-y divide-gray-200 dark:divide-gray-700 dark:text-white">
+                            <tbody
+                                class="bg-white dark:bg-gray-900 divide-y divide-gray-200 dark:divide-gray-700 dark:text-white">
                                 @foreach ($followUps as $followUp)
-                                    <tr class="hover:bg-gray-50 transition duration-300 dark:hover:bg-gray-800">
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            {{ $followUp->created_at->format('d M Y, h:i A') }}
-                                        </td>
-                                        {{-- <td class="px-6 py-4 whitespace-nowrap">
+                                    @if ($followUp->patient) <!-- Skip if patient is null -->
+                                        <tr class="hover:bg-gray-50 transition duration-300 dark:hover:bg-gray-800">
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                {{ $followUp->created_at->format('d M Y, h:i A') }}
+                                            </td>
+                                            {{-- <td class="px-6 py-4 whitespace-nowrap">
                                             <a href="{{ route('patients.show', $followUp->patient->id) }}"
                                                 class="text-indigo-600 hover:text-indigo-900">{{ $followUp->patient->name }}</a>
                                         </td> --}}
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            <!-- Ensure that $followUp->patient is not null before accessing its id -->
-                                            @if ($followUp->patient)
-                                                <a href="{{ route('patients.show', $followUp->patient->id) }}"
-                                                   class="text-indigo-600 hover:text-indigo-900">
-                                                    {{ $followUp->patient->name }}
-                                                </a>
-                                            @else
-                                                <!-- Fallback if patient is not found -->
-                                                <span class="text-gray-400">No Patient</span>
-                                            @endif
-                                        </td>
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <!-- Ensure that $followUp->patient is not null before accessing its id -->
+                                                @if ($followUp->patient)
+                                                    <a href="{{ route('patients.show', $followUp->patient->id) }}"
+                                                        class="text-indigo-600 hover:text-indigo-900">
+                                                        {{ $followUp->patient->name }}
+                                                    </a>
+                                                @else
+                                                    <!-- Fallback if patient is not found -->
+                                                    <span class="text-gray-400">No Patient</span>
+                                                @endif
+                                            </td>
 
 
-                                        <td class="px-6 py-4 text-gray-600 dark:text-gray-300"
-                                        style="vertical-align: top;">
-                                        @if ($followUp->check_up_info)
-                                            @php
-                                                $checkUpInfo = json_decode($followUp->check_up_info, true);
-                                            @endphp
+                                            <td class="px-6 py-4 text-gray-600 dark:text-gray-300"
+                                                style="vertical-align: top;">
+                                                @if ($followUp->check_up_info)
+                                                    @php
+                                                        $checkUpInfo = json_decode($followUp->check_up_info, true);
+                                                    @endphp
 
-                                            {{-- <div>
-                                                @if (isset($checkUpInfo['payment_method']) &&
-                                                        $checkUpInfo['payment_method'] !== null &&
-                                                        $checkUpInfo['payment_method'] !== '')
+                                                    {{-- <div>
+                                                @if (isset($checkUpInfo['payment_method']) && $checkUpInfo['payment_method'] !== null && $checkUpInfo['payment_method'] !== '')
                                                     <p class="mt-2">
                                                         <span
                                                             class="font-bold text-gray-800 dark:text-gray-200">{{ __('messages.Payment Method') }}:</span>
@@ -73,16 +73,16 @@
 
                                             </div> --}}
 
-                                            <div>
-                                                @if (isset($checkUpInfo['amount']) && $checkUpInfo['amount'] !== null && $checkUpInfo['amount'] !== '')
-                                                    <p class="mt-2">
-                                                        <span
-                                                            class="font-bold text-gray-600 dark:text-gray-200">{{ __('') }}</span>
-                                                        {{ $checkUpInfo['amount'] }}
-                                                    </p>
-                                                @endif
+                                                    <div>
+                                                        @if (isset($checkUpInfo['amount']) && $checkUpInfo['amount'] !== null && $checkUpInfo['amount'] !== '')
+                                                            <p class="mt-2">
+                                                                <span
+                                                                    class="font-bold text-gray-600 dark:text-gray-200">{{ __('') }}</span>
+                                                                {{ $checkUpInfo['amount'] }}
+                                                            </p>
+                                                        @endif
 
-                                                {{-- @if (isset($checkUpInfo['balance']) && $checkUpInfo['balance'] !== null && $checkUpInfo['balance'] !== '')
+                                                        {{-- @if (isset($checkUpInfo['balance']) && $checkUpInfo['balance'] !== null && $checkUpInfo['balance'] !== '')
                                                     <p>
                                                         <span
                                                             class="font-bold text-gray-600 dark:text-gray-200">{{ __('messages.Balance') }}:</span>
@@ -91,11 +91,12 @@
                                                 @endif --}}
 
 
-                                            </div>
-                                        @endif
-                                    </td>
+                                                    </div>
+                                                @endif
+                                            </td>
 
-                                    </tr>
+                                        </tr>
+                                    @endif
                                 @endforeach
 
                             </tbody>
