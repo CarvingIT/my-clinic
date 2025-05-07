5c8d1f0d1c4f9fcbf850ba7e3dc1e3109cecce32
Ketan Kulkarni <ketan@carvingit.com>
2025-05-07 10:01:43 +0530

    minor updates

--- packages/carving-i-t/laravel-user-roles/src/App/Traits/LaravelUserRoles.php
+++ packages/carving-i-t/laravel-user-roles/src/App/Traits/LaravelUserRoles.php
@@ -6,11 +6,26 @@ use CarvingIT\LaravelUserRoles\App\Models\Role;
 use CarvingIT\LaravelUserRoles\App\Models\UserRole;
 
 trait LaravelUserRoles{
+    public function user_roles(){
+            return $this->hasMany(UserRole::class);
+    }
+
     public function roles(){
-            return $this->hasManyThrough(Role::class, UserRole::class);
+        $user_roles = $this->user_roles;
+         return $user_roles->map(function($user_role){
+                return $user_role->role;
+         });
+    }
+
+    public function hasRole($role_name){
+        $roles = $this->roles();
+        foreach($roles as $r){
+            if($r->name == $role_name) return true;
         }
+        return false;
+    }
     
-    public function assignRole($role_names){
+    public function assignRoles($role_names){
         foreach($role_names as $role_name){
             $role = Role::where('name', $role_name)->first();
             if($role && !empty($this->id)){
@@ -29,7 +44,7 @@ trait LaravelUserRoles{
         }
     }
 
-    public function unassignRole($role_names){
+    public function unassignRoles($role_names){
         foreach($role_names as $role_name){
             $role = Role::where('name', $role_name)->first();
             UserRole::where('role_id', $role->id)->delete();
