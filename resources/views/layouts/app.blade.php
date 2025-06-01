aedb52488b8233492b18d9b9094bc9cc0915db11
dhananj001 <dhananjayborse001@gmail.com>
2025-06-01 10:02:51 +0530

    Favicon added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
+        <title>My Clinic</title>
+
+        <!-- Favicon -->
+        <link rel="icon" href="{{ asset('favicon.png') }}">
+
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
