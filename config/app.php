cc9112fa5b34273436443c7d2f8b1fae2a0c834c
Dhananjay Borse <dhananjayborse001@gmail.com>
2026-04-21 14:25:55 +0530

    login page shows clinic name from env

--- config/app.php
+++ config/app.php
@@ -14,6 +14,8 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    
+    'clinic_name' => env('CLINIC_NAME', 'My Clinic'),
 
     /*
     |--------------------------------------------------------------------------
